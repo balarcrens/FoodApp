1778039fb332a97d419c14d566404ee39c6e3499
Balar Crens <balarcrens@gmail.com>
2025-05-06 18:12:52 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost:27017/food?directConnection=true';
+const URI = 'mongodb+srv://balarcrens188:crens446@cluster0.abcde.mongodb.net/food?retryWrites=true&w=majority';
 
 const mongodb = async () => {
     try {
