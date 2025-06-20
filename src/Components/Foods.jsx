a68cff5c0ef2c1c1d0b890feefca672ea8ff1f42
Balar Crens <balarcrens@gmail.com>
2025-06-20 21:13:10 +0530

    Initial commit

--- src/Components/Foods.jsx
+++ src/Components/Foods.jsx
@@ -71,7 +71,7 @@ export default function Foods() {
                         <div aria-hidden="true" className="absolute inset-x-0 top-[-15rem] -z-10 transform-gpu overflow-hidden blur-2xl sm:top-[-25rem]">
                             <div style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)', }} className="relative left-1/2 -z-10 aspect-1155/678 w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-50 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" />
                         </div>
-                        <div className="mx-auto max-w-sm sm:max-w-3xl lg:max-w-4xl xl:max-w-7xl py-16 px-8 lg:px-8" ref={foodSectionRef}>
+                        <div className="mx-auto max-w-3xl lg:max-w-4xl xl:max-w-7xl py-8 sm:py-16 px-4 sm:px-8 lg:px-8" ref={foodSectionRef}>
                             <div className="flex flex-wrap justify-evenly sm:justify-between w-full gap-2 sm:w-auto mb-5">
                                 <h2 className="text-2xl font-bold text-center my-auto sm:flex-none sm:mx-0"> Foods </h2>
                                 <div className="relative">
@@ -86,24 +86,24 @@ export default function Foods() {
                                 </div>
                             ) : (
                                 <>
-                                    <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                                    <div className="grid gap-x-4 gap-y-6 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                                         {currentFoods.map((food, index) => (
                                             <div key={food._id} className="group cursor-pointer" data-aos="fade-up" data-aos-duration={`${1500 + index * 100}`} data-aos-delay={`${index * 100}`} data-aos-easing="ease" data-aos-anchor-placement="top-bottom" >
                                                 <Link to={`/food/${food._id}`} className="btn w-full">
-                                                    <img src={food.img} alt={food.name + " " + food.description} className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75" />
                                                     <div className="relative">
+                                                        <img src={food.img} alt={food.name + " " + food.description} className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75" />
                                                         <button onClick={async (e) => {
                                                             e.preventDefault();
                                                             await toggleFavourite(food._id);
                                                             await fetchFavourite();
-                                                        }} className={`absolute right-2 ${localStorage.getItem('theme') === 'dark' ? 'text-white' : 'text-black'} z-10 cursor-pointer hover:scale-[1.3] transition-all`} >
+                                                        }} className={`absolute top-2 right-2 text-white z-10 cursor-pointer hover:scale-[1.1] transition-all`} >
                                                             <i className={`${(favourites || []).includes(food._id) ? 'fa-solid text-red-500 hover:text-red-600 fa-beat' : 'fa-regular'} fa-heart fa-lg`}></i>
                                                         </button>
-                                                        <h3 className="mt-4">{food.name}</h3>
-                                                        <div className="mt-2 flex text-lg font-medium">
-                                                            <s className="text-red-500">₹{Math.ceil(food.price + (food.price * 0.10))}/-</s>
-                                                            <p className="mx-2" data-aos="fade-right">₹{food.price}/- </p>
-                                                        </div>
+                                                    </div>
+                                                    <h3 className="mt-2 sm:mt-4">{food.name}</h3>
+                                                    <div className="mt-2 flex gap-0.5 text-sm sm:text-lg font-medium flex-wrap">
+                                                        <s className="text-red-500">₹{Math.ceil(food.price + (food.price * 0.10))}/-</s>
+                                                        <p data-aos="fade-right">₹{food.price}/- </p>
                                                     </div>
                                                 </Link>
                                             </div>
